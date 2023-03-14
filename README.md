d3dafe4bc26eff9d9b53e4d658e6e3c1abb31c32
ROWANMENG <mqy2021official@163.com>
2023-03-14 04:26:44 +0000

    version 1.1

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ENGF0002-scenarios-2
 This is a repository to host the API code for the Logic circuit simulator application built as part of the ENGF0002 module 🚀
 
+## version 1.1
+add new key file(this file should not be changed)
+
+modify the data structure used, which is shown in db.json
+
+what should be up to change: 
+due to the new data structure, the algorithm used to calculate should be updated
--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, jsonify, request
 import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('serviceAccountKey.json')
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 operators = ["AND", "OR", "NAND", "NOT"]
 app = Flask(__name__)
@@ -70,11 +80,6 @@ def write_to_file(data):
         f.write(json.dumps(json_file))
 
 
-@app.get("/test")
-def home():
-    print(request.get_json()["text"])
-    return jsonify({"message": "Hello :)"})
-
 
 @app.get("/calculate")
 def calculate_truth_table():
@@ -86,33 +91,65 @@ def calculate_truth_table():
         {"truth_table": gen_truthtable(int(number_of_inputs)), "output": result}
     )
 
-
+# post information about some new cirsuits. If the cirsuir is saved, return the id of the circuit
 @app.post("/save")
 def save():
-    data = request.get_json()
-    expression = data["expression"]
-    number_of_inputs = data["inputs"]
-    result = calc_circuit_table(gen_truthtable(int(number_of_inputs)), expression)
-    data["result"] = result
-    data["truth_table"] = gen_truthtable(int(number_of_inputs))
-    write_to_file(data)
-    return jsonify({"message": "Saved!"})
+    circuits = request.get_json()
+    id_ignore = []
+    for i in range(len(circuits)):
+        if db.collection(u'preset_circuits').document(str(circuits[i]["id"])).get().exists:
+            id_ignore.append(circuits[i]["id"])
+            continue
+        else:
+            doc_ref = db.collection(u'preset_circuits').document(str(circuits[i]["id"]))
+            doc_ref.set(circuits[i])
+    if len(id_ignore) == 0:
+        return {'success': "All circuits are saved!"}
+    else:
+        return {'success': "All circuits are saved!", 'ignore': id_ignore}
 
+# update some circuits, if the circuit is not exist, return the id of the circuit
+@app.put("/update")
+def update():
+    circuits = request.get_json()
+    id_not_exist = []
+    for i in range(len(circuits)):
+        if not db.collection(u'preset_circuits').document(str(circuits[i]["id"])).get().exists:
+            id_not_exist.append(circuits[i]["id"])
+            continue
+        else:
+            doc_ref = db.collection(u'preset_circuits').document(str(circuits[i]["id"]))
+            doc_ref.set(circuits[i])
+    if len(id_not_exist) == 0:
+        return {'success': "All circuits are saved!"}
+    else:
+        return {'success': "All circuits are saved!", 'not exist': id_not_exist}
 
+# get all circuits
 @app.get("/get")
 def get():
-    file = open("./db.json", "r")
-    json_file = json.load(file)
-    file.close()
-    return jsonify(json_file)
+    docs = db.collection(u'preset_circuits').stream()
+    circuits = []
+    for doc in docs:
+        circuits.append(doc.to_dict())
+    if len(circuits) == 0:
+        return {'not exist': "No circuits!"}
+    else:
+        return jsonify(circuits)
 
-
+# get a circuit by one id, if the circuit is not exist, return the id of the circuit
 @app.get("/get/<id>")
 def get_by_id(id):
-    file = open("./db.json", "r")
-    json_file = json.load(file)
-    file.close()
-    return jsonify(json_file[id])
+    id_not_exist = None
+    if not db.collection(u'preset_circuits').document(str(id)).get().exists:
+        id_not_exist = id
+    else:
+        circuit = []
+        circuit.append(doc = db.collection(u'preset_circuits').document(str(id)).get().to_dict())
+    if id_not_exist is None:
+        return jsonify(circuit)
+    else:
+        return {'not exist': id_not_exist}
 
 
 if __name__ == "__main__":
